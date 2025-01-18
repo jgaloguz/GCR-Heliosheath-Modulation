e707ea7ddb5d453e6cc5608fde062bc439f5cf75
Juan G Alonso Guzman <jgaloguz@gmail.com>
2025-01-18 11:49:56 -0600

    Prepare repository for application
    The fluid, geodesic, geometry, tests, and benchmark folders have been deleted since they will not be used for this application. Also, a results folder was generated to dump all the simulation, analysis, and plotting results. This folder will remain empty in the public repository. A configure.sh script was also created to expedite configuration with autotools.

---
+++ configure.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+autoreconf
+automake --add-missing
+./configure CXXFLAGS="-Ofast" --with-mpi="$1" --with-execution=PARALLEL --with-trajectory=PARKER --with-time_flow=BACKWARD --with-rkmethod=0 --with-server=SELF
---
+++ results/README.md
@@ -0,0 +1,1 @@
+This folder will contain all simulation, analysis, and plotting results.
