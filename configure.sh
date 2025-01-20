b196e40cda38035a5f1ca520a7c78d86d8a6bfe8
Juan G Alonso Guzman <jgaloguz@gmail.com>
2025-01-20 12:20:30 -0600

    Update configure.sh
    Took off quotations around MPI option since this was not properly handled in some systems.

--- configure.sh
+++ configure.sh
@@ -2,4 +2,4 @@
 
 autoreconf
 automake --add-missing
-./configure CXXFLAGS="-Ofast" --with-mpi="$1" --with-execution=PARALLEL --with-trajectory=PARKER --with-time_flow=BACKWARD --with-rkmethod=0 --with-server=SELF
+./configure CXXFLAGS="-Ofast" --with-mpi=$1 --with-execution=PARALLEL --with-trajectory=PARKER --with-time_flow=BACKWARD --with-rkmethod=0 --with-server=SELF
