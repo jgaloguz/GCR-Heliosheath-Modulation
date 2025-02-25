fe36debbc7590ca91425dc314851c085a7cb13d4
Juan G Alonso Guzman <jgaloguz@gmail.com>
2025-02-25 11:43:53 -0600

    Added WSO tilt angle plot routine
    Added data and Python code to fit and plot current sheet tilt angle using WSO measurements.

---
+++ runs/plot_WSO_tilt_angle.py
@@ -0,0 +1,81 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+from scipy.optimize import curve_fit
+from datetime import datetime as dt
+import time
+
+# Cubic stretch function
+def CubicStretch(t, d):
+   return t * ((1.0 - d) * (t / np.pi)**2 + 3.0 * (d - 1.0) * (t / np.pi) + 3.0 - 2.0 * d)
+
+# Periodic function
+def HCS(t, p0, A, T, t0, d):
+   arg = 4.0 * np.pi * (t - t0) / T
+   arg_mod = np.floor(arg / (2.0 * np.pi))
+   arg_rem = CubicStretch(arg - arg_mod * 2.0 * np.pi, d)
+   return p0 + A * np.cos(arg_rem)
+
+def toYearFraction(date):
+   def sinceEpoch(date): # returns seconds since epoch
+      return time.mktime(date.timetuple())
+   s = sinceEpoch
+
+   year = date.year
+   startOfThisYear = dt(year=year, month=1, day=1)
+   startOfNextYear = dt(year=year+1, month=1, day=1)
+
+   yearElapsed = s(date) - s(startOfThisYear)
+   yearDuration = s(startOfNextYear) - s(startOfThisYear)
+   fraction = yearElapsed/yearDuration
+
+   return date.year + fraction
+
+# Import data
+CRi = 1642
+CRf = 2085
+year_float = []
+CR = []
+R_av = []
+R_n = []
+R_s = []
+
+file = open("data/WSO_tilt_angle.dat", "r")
+# Header lines
+for row in range(1):
+   line = file.readline()
+# Data
+line = file.readline()
+while line:
+   data_str = line.split()
+   CR.append(int(data_str[1])) # Carrington rotation number
+   date = dt.strptime(data_str[2],"%Y:%m:%d")
+   year_float.append(toYearFraction(date))
+   R_av.append(float(data_str[4]))
+   R_n.append(float(data_str[5]))
+   R_s.append(-float(data_str[6]))
+   line = file.readline()
+file.close()
+year_float = np.array(year_float)
+
+opt_params, cov = curve_fit(HCS, year_float, R_av,
+                            p0=[40.0, 35.0, 22.0, 2002.0, 0.5], maxfev=10000,
+                            bounds=([40.0, 35.0, 21.9, 2002.0, 0.0], [40.1, 35.1, 22.0, 2002.1, 1.0]))
+
+print("Optimal Fit Parameters:", opt_params)
+
+# Plot
+fig = plt.figure(figsize=(12, 10), layout='tight')
+ax = fig.add_subplot(111, projection='rectilinear')
+
+ax.plot(year_float, R_av, linestyle="", marker="o", label="average", markersize=8)
+ax.plot(year_float, HCS(year_float, *opt_params), label="fit", linewidth=4)
+ax.set_xlabel('Year', fontsize=30)
+ax.set_ylabel('Computed HCS Tilt Angle ($^\\circ$)', fontsize=30)
+ax.set_ylim(0.0,90.0)
+ax.tick_params(labelsize=30)
+ax.legend(fontsize=20)
+
+plt.show()
+plt.close(fig)
